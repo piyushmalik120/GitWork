6f1dd3b898f0b40a47599e60ec8052ea0261849e
piyushmalik120 <pmalik6@sapient.com>
2015-02-06 16:25:07 +0530

    teds

--- src/com/sapient/work/MyClass.java
+++ src/com/sapient/work/MyClass.java
@@ -5,7 +5,7 @@ public class MyClass {
 	public static void main(String args[]) throws Exception {
 
 
-		System.out.println("This is My Class yess yesss noo");
+		System.out.println("This");
 
 	}
 
